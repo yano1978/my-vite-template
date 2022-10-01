361f789e6441d3aee5e075ee77d482b94c2d2108
yano1978 <yano1978@gmail.com>
2022-10-01 18:34:12 +0100

    Plugin moved to a separate file

--- src/main.js
+++ src/main.js
@@ -1,14 +1,9 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import { myFirstPlugin } from "./myFirstPlugin";
 
 import "./assets/main.css";
-
-const myFirstPlugin = {
-  install(app, options) {
-    console.log("Hello plugin", options);
-  },
-};
 
 const app = createApp(App).use(myFirstPlugin, {
   option1: true,
---
+++ src/myFirstPlugin/index.js
@@ -0,0 +1,9 @@
+// export const myFirstPlugin = {
+//   install(app, options) {
+//     console.log("Hello plugin", options);
+//   },
+// };
+
+export function myFirstPlugin(app, options) {
+  console.log("Hello plugin", options);
+}
