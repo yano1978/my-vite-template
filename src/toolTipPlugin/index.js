15b763771027945a31faffded10758edecce22a0
yano1978 <yano1978@gmail.com>
2022-10-01 19:37:38 +0100

    tooltip options overriding globally + defu dependency added to merge array and objects options

--- src/toolTipPlugin/index.js
+++ src/toolTipPlugin/index.js
@@ -1,7 +1,13 @@
 import { defineAsyncComponent } from "vue";
+import { defu } from "defu";
 export const tooltipOptionsInject = Symbol();
 
 export function toolTipPlugin(app, options) {
+  options = defu(options, {
+    arrow: false,
+    ...options,
+  });
+
   app.provide(tooltipOptionsInject, options);
   app.component(
     "ToolTip",
